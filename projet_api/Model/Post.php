e6ca63f77ac880960e6752098e56827f0cf4953e
hugostarte <47392672+hugostarte@users.noreply.github.com>
2023-03-30 10:51:19 +0200

    Update Post.php

--- projet_api/Model/Post.php
+++ projet_api/Model/Post.php
@@ -9,20 +9,6 @@ class Post extends Model
     {
         parent::__construct("post");
     }
-
-    public function getComments($object)
-    {
-        $req = $this->db->prepare("SELECT * FROM commentaires WHERE post_id=?");
-        $req->execute(array($object->id));
-        $req->setFetchMode(\PDO::FETCH_OBJ);
-        return $req->fetchAll();
-    }
-    public function updateComments($content,$id){
-        $req = $this->db->prepare("UPDATE commentaires SET content=? WHERE post_id=?");
-        $req->execute(array($content,$id));
-        $req->setFetchMode(\PDO::FETCH_OBJ);
-        return $req->fetchAll();
-    }
     
 }
 
