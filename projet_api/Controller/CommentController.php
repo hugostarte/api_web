1774cfb3c0b3f2e0a7c5a3aa6e8a7aa7e34d749c
KamelAch <114158345+KamelAch@users.noreply.github.com>
2023-03-30 10:32:41 +0200

    Ajout du controller pour les commentaires

---
+++ projet_api/Controller/CommentController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Controller;
+date_default_timezone_set('Europe/Paris');
+use stdClass;
+
+class CommentController extends Controller
+{
+    private $commentManager;
+
+    public function __construct()
+    {
+        $this->commentManager = new \Model\Post();
+    }
+
+    function getAll()
+    {
+        $this->JSON($this->commentManager->getAll());
+        $listuser = $this->commentManager->getAll();
+        $this->addViewParams("comment",$listcomment);
+        $this->View("listcomment");
+    }
+
+    function getOne($id)
+    {
+        $this->JSON($this->commentManager->getOne($id));
+        $this->JSON($this->commentManager->getComments($id)); 
+    }
+
+    function create()
+    {
+        $comment = new \stdClass();
+        $comment->firstname = htmlentities($_POST["firstname"]);
+        $comment->lastname =  htmlentities($_POST["lastname"]);
+        $comment->lastname =  htmlentities($_POST["post_id"]);
+        $comment->content = htmlentities($_POST["content"]);
+        $comment->commentdate = date("m-d-Y H:i:s"); // Enregistre en bd la date et l'heure courante
+        $this->commentManager->create($comment);
+
+        $this->JSONMessage("Votre commentaire a été ajouté");
+    }
+
+    function update($id)
+    {
+        $auth = getAuth();
+        if($auth){
+            if($auth->id == $id){
+                $data = json_decode(file_get_contents("php://input"));
+                $comment = new \stdClass();
+                $comment->id = $id;
+                $comment->email = $data->email;
+                if ($this->commentManager->update($comment)) {
+                    $this->JSONMessage("Commentaire mis à jour");
+                } else {
+                    $this->JSONMessage("Commentaire non trouvé");
+                }
+            }else{
+                $this->JSONMessage("Vous n'avez pas les droits pour modifier ce commentaire");
+            }
+        }
+        
+    }
+
+    function delete($id)
+    {
+        if ($this->commentManager->delete($id)) {
+            $this->JSONMessage("Post supprimé");
+        } else {
+            $this->JSONMessage("Post non trouvé");
+        }
+    }
+    
+
+
+   
+}
